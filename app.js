fd38ff19cfd801ffbeb6708f9542d053486cb92e
AbhishekSrivastav <abhisheksrivastav596@gmail.com>
2026-02-02 10:56:30 +0530

    Feat: Added Middleware

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 
 app.use(cors({ origin: env.clientUrl }));
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
 
 app.use("/api/stocks", stockRoutes);
 app.use("/api/watchlist", watchlistRoutes);
